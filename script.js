8f296bd879b2294b6f137b5807955d38deee807c
Daniel Bonn <danieltbonn@gmail.com>
2023-06-24 17:01:48 -0500

    Added skeleton code for functions to get the password length, character types, and to generate

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 // Assignment code here
 
+// Pass length must be between 8 and and 128 characters inclusive
+function getPasswordLength() {
+  var len = 8;
+  return len;
+}
+
+// Character types may include lowercase, uppercase, numeric, and/or special characters
+function getCharacters() {
+  var chars = [];
+  return chars;
+}
+
+// Generates password by passing in functions
+function generatePassword(len, char) {
+  return;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(getPasswordLength(), getCharacters());
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
